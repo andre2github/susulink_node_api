3033352f726997c0d97552b036d6df3fd3fa1547
motto <motto2email@163.com>
2018-06-30 11:44:52 +0800

    nothing

--- src/app.js
+++ src/app.js
@@ -1,7 +1,21 @@
-var path = require('path'),
-    express = require('express'),
-    env = require(path.resolve(__dirname, 'config', 'env'))[process.env.run_mode],
-    app = express();
+var path = require('path')
+    , fs = require('fs')
+    , express = require('express')
+    , MarkdownIt = require('markdown-it')
+    , md = new MarkdownIt()
+    , env = require(path.resolve(__dirname, 'config', 'env'))[process.env.run_mode]
+    , app = express();
+
+app.get('/', function (req, res, next) {
+    fs.readFile(path.resolve(__dirname, '..', 'README.md'), 'utf-8', function (err, data) {
+        if (err) {
+            res.status(500).send(err.stack);
+        } else {
+            var result = md.render(data);
+            res.send(result);
+        }
+    });
+});
 
 app.use('/api/common_link', require(path.resolve(__dirname, 'routers', 'common_link')));
 
