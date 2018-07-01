d63d3adfc084ffc8f0aefa55b73ee794cdadb5c5
motto <motto2email@163.com>
2018-07-01 10:17:11 +0800

    nothing

--- src/utils/db.js
+++ src/utils/db.js
@@ -9,24 +9,24 @@ var db = {
         if (!db._pool) {
             db._pool = mysql.createPool(env.mysql);
 
-            // {
-            //     // 该事件 当池中创建新的连接槽时 触发
-            //     db._pool.on('connection', function (connection) {
-            //         console.log('Connection %d connection', connection.threadId);
-            //     });
-            //     // 该事件 当从池中获取连接槽时 触发
-            //     db._pool.on('acquire', function (connection) {
-            //         console.log('Connection %d acquired', connection.threadId);
-            //     });
-            //     // 该事件 当把连接槽放入池中时 触发
-            //     db._pool.on('release', function (connection) {
-            //         console.log('Connection %d released', connection.threadId);
-            //     });
-            //     // 该事件 当池中连接槽不够用 等待可用连接槽时 触发
-            //     db._pool.on('enqueue', function () {
-            //         console.log('Waiting for available connection slot');
-            //     });
-            // }
+            {
+                // 该事件 当池中创建新的连接槽时 触发
+                db._pool.on('connection', function (connection) {
+                    console.log('Connection %d connection', connection.threadId);
+                });
+                // 该事件 当从池中获取连接槽时 触发
+                db._pool.on('acquire', function (connection) {
+                    console.log('Connection %d acquired', connection.threadId);
+                });
+                // 该事件 当把连接槽放入池中时 触发
+                db._pool.on('release', function (connection) {
+                    console.log('Connection %d released', connection.threadId);
+                });
+                // 该事件 当池中连接槽不够用 等待可用连接槽时 触发
+                db._pool.on('enqueue', function () {
+                    console.log('Waiting for available connection slot');
+                });
+            }
 
         }
         return db._pool;
